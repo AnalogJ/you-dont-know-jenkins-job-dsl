d50789c4afc53e94b78951f93b65baf43d391094
Jason Kulatunga <jk17@ualberta.ca>
2016-08-17 20:35:44 -0700

    description is required.

--- factory_pattern_example_dsl.groovy
+++ factory_pattern_example_dsl.groovy
@@ -3,7 +3,7 @@ import companyname.factory.*
 
 def buildJobFactory = new BuildJobFactory(this)
 buildJobFactory.buildWebAppRpm()
-buildJobFactory.baseBuildRpmJob('Build-Dynamically-Defined-Rpm')
+buildJobFactory.baseBuildRpmJob('Build-Dynamically-Defined-Rpm', 'dynamic job description ')
     .with{
         scm {
             // your scm (git/hg/perforce/..) repo config here
